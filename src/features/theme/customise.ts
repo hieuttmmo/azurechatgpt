b8438fb9b4b29bd1585425cce87e223a680b18b0
Hieu Tran <46371125+hieuttmmo@users.noreply.github.com>
2023-09-21 11:34:25 +0700

    Update customise.ts

--- src/features/theme/customise.ts
+++ src/features/theme/customise.ts
@@ -1,1 +1,2 @@
-export const AI_NAME = "ChatGPT on Azure solution accelerator";
+// export const AI_NAME = "ChatGPT on Azure solution accelerator";
+export const AI_NAME = "RWE GPT on Azure OpenAI";
